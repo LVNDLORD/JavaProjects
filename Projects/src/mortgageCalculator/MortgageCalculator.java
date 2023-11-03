e586c5c7275d450aad00aa33514ca051a0aed96d
Andrey <andydeshko@gmail.com>
2023-11-03 22:40:11 +0200

    added conditions to mortgage calculator

--- Projects/src/mortgageCalculator/MortgageCalculator.java
+++ Projects/src/mortgageCalculator/MortgageCalculator.java
@@ -16,14 +16,47 @@ public class MortgageCalculator {
     }
 
     public static void main(String[] args) {
+        final int MIN_AMOUNT = 1000;
+        final int MAX_AMOUNT = 1_000_000;
+        final double MIN_INTEREST = 0.0;
+        final double MAX_INTEREST = 30.0;
+        final int MIN_YEARS = 1;
+        final int MAX_YEARS = 30;
+        int principal = 0;
+        double annualInterest = 0.0;
+        byte years = 0;
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Amount of loan: ");
-        int principal = Integer.parseInt(scanner.nextLine());
-        System.out.print("Annual interest rate: ");
-        double annualInterest = Double.parseDouble(scanner.nextLine());
-        System.out.print("Period (Years): ");
-        byte years = Byte.parseByte(scanner.nextLine());
+        while (true) {
+            System.out.print("Amount of loan ($1K - $1M): ");
+            principal = Integer.parseInt(scanner.nextLine());
+            if (principal >= MIN_AMOUNT && principal <= MAX_AMOUNT) {
+                break;
+            }
+            System.out.printf("Enter a number between %s and %s%n", MIN_AMOUNT, MAX_AMOUNT);
+
+        }
+
+        while (true) {
+            System.out.print("Annual interest rate: ");
+            annualInterest = Double.parseDouble(scanner.nextLine());
+            if (annualInterest > MIN_INTEREST && annualInterest <= MAX_INTEREST) {
+                break;
+            }
+            System.out.printf("Enter a value greater than %s and less than or equal to %s.%n", MIN_INTEREST, MAX_INTEREST);
+
+        }
+
+        while (true) {
+            System.out.print("Period (Years): ");
+            years = Byte.parseByte(scanner.nextLine());
+            if (years >= MIN_YEARS && years <= MAX_YEARS) {
+                break;
+            }
+            System.out.printf("Enter a value between %s and %s.%n", MIN_YEARS, MAX_YEARS);
+        }
+        scanner.close();
 
         NumberFormat currency = NumberFormat.getCurrencyInstance();
         String result = currency.format(calculateMonthlyMortgage(principal, annualInterest, years));
